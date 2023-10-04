edd7a8a8fe97e521d4fc262fbd31ddd6a71108a8
kevinSantos96 <95662792+kevinSantos96@users.noreply.github.com>
2023-10-03 19:15:27 -0600

    busqueda por id

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -1,15 +1,34 @@
 import {conn} from '../dbConnectiion.js'
 
-export const getEmployees = (req,res)=>res.send('Obteniendo empleados')
+export const getEmployees = async(req,res)=>{
+
+    const [result] = await conn.query('SELECT * FROM tbl_Empleados')
+    res.status(200).json({
+        message: 'OK',
+        success: true,
+        result
+    })
+}
 
 export const createEmployes = async (req,res)=>{
     const {name, lastName,Gender,Phone,Email,Status} = req.body
 
    const [result] = await conn.query('INSERT INTO tbl_Empleados(Nombres,Apellidos,Fecha,Genero,Telefono,Correo,Estado) VALUES(?,?,curdate(),?,?,?,?)',
    [name,lastName,Gender,Phone,Email,Status])
-   res.json({
+   res.status(200).json({
     message: 'OK',
-    status: 200,
+    success: true,
     id: result.insertId
    })
+}
+
+export const getEmployeeById = async(req,res)=>{
+    const {id} = req.params
+    const [result] =  await conn.query('SELECT * FROM tbl_Empleados WHERE Id = ?',[id])
+
+    res.status(200).json({
+        message: 'ok',
+        success: true,
+        result
+    })
 }
--- src/routes/employes.routes.js
+++ src/routes/employes.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { createEmployes, getEmployees } from "../controllers/employees.controller.js";
+import { createEmployes, getEmployeeById, getEmployees } from "../controllers/employees.controller.js";
 
 const router = Router()
 
 router.get('/employees',getEmployees)
 router.post('/create/employee',createEmployes)
-
+router.get('/get/employee/:id',getEmployeeById)
 export default router
